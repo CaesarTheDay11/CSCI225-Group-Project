ab7ceb2f36cc0eeec67d05f170e93284c8009112
Jacob <1jepps10@gmail.com>
2025-11-22 19:29:13 -0500

    seperated pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,30 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js'
+import { auth, db, } from "./firebase.js";
 
+/*
 import {
-  getFunctions,
   connectFunctionsEmulator,
   httpsCallable
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-functions.js";
+*/
 
 import {
-  getDatabase,
   onValue,
-  connectDatabaseEmulator,
   ref, set,
+  onDisconnect,
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js"
 
 import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
   onAuthStateChanged,
-  connectAuthEmulator
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCAZCdpLSNAHhJMbX6XO7kM6OGEq1Wlp_g",
-  authDomain: "arena-battlegrounds-2724e.firebaseapp.com",
-  projectId: "arena-battlegrounds-2724e",
-  storageBucket: "arena-battlegrounds-2724e.firebasestorage.app",
-  messagingSenderId: "772072071103",
-  appId: "1:772072071103:web:0ecb0d98cdbe1d39708b0a",
-  measurementId: "G-G1Q86QFQBP",
-  databaseURL: "https://arena-battlegrounds-2724e-default-rtdb.firebaseio.com/"
-};
-
-
-// Initialize Firebase and services
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-const functions = getFunctions();
-
-connectFunctionsEmulator(functions, "localhost", 5001);
-connectAuthEmulator(auth, "http://127.0.0.1:9199");
-connectDatabaseEmulator(db, "127.0.0.1", 9009);
-
+// setup connection listener
 const connectedRef = ref(db, ".info/connected");
 onValue(connectedRef, (snap) => {
   console.log("connected:", snap.val());
 });
 
-// auth setup
-const provider = new GoogleAuthProvider();
-
-document.getElementById("googleBtn").onclick = async () => {
-  try {
-    await signInWithPopup(auth, provider);
-  } catch (e) {
-    console.error(e);
-  }
-};
-
 var uid
+var queueRef;
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
@@ -69,13 +34,24 @@ onAuthStateChanged(auth, (user) => {
     writeUserData(user.uid, user.displayName);
     uid = user.uid;
     const matchRef = ref(db, "users/" + uid + "/currentMatch");
+    queueRef = ref(db, 'queue/' + uid);
+    onDisconnect(queueRef).remove();
+
+    document.querySelectorAll('.not-logged-in-vis')
+      .forEach(el => el.style.display = 'none');
+
+    console.log(user);
+
 
     onValue(matchRef, snap => {
       if (snap.exists()) {
+        document.getElementsByClassName("queueBtn")[0].textContent = "Match Found!";
         console.log("Matched! Match ID:", snap.val());
         // TODO: load match screen
       }
     });
+
+
   } else {
     document.getElementById("user").textContent = "Not signed in";
   }
@@ -89,7 +65,8 @@ function writeUserData(userId, name) {
 
 function updateQueueData() {
   // TODO: store rank value later for skill based matchmaking
-  set(ref(db, 'queue/' + uid), { "UserID:": uid });
+  document.getElementsByClassName("queueBtn")[0].textContent = "Finding a Match...";
+  set(queueRef, { "UserID:": uid });
 }
 
 document.getElementById("queueBtn").addEventListener("click", updateQueueData, false);
